337c913d4fff0366085e6a03a8eab459005e50b7
angelrakic <andjela.rakic@orioninc.com>
2021-10-12 02:14:56 +0200

    Ne daj Boze nikome

---
+++ Zadatak3_userDefaults/Store.swift
@@ -0,0 +1,16 @@
+//
+//  Store.swift
+//  Zadatak3_userDefaults
+//
+//  Created by Andjela Rakic on 10/11/21.
+//
+
+import Foundation
+
+struct Store: Codable {
+    
+    let name: String
+    let city: String
+    let storeLogoURL: String
+    
+}
